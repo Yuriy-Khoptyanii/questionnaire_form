40fc7c4e9e1b1ccff940d708dc08791c03389c3d
Yuriy Khoptyanii <hoptyanoyyura@gmail.com>
2023-06-13 19:51:33 +0300

    fixed some logic

--- src/components/registrationForm/RegistrationForm.tsx
+++ src/components/registrationForm/RegistrationForm.tsx
@@ -2,8 +2,9 @@ import './RegistrationForm.scss';
 import 'react-phone-input-2/lib/style.css';
 
 import axios from 'axios';
-import { ErrorMessage, Field, FieldProps, Form, Formik, FormikValues } from 'formik';
-import PhoneInput from 'react-phone-input-2';
+import { ErrorMessage, Field, Form, Formik } from 'formik';
+import { useState } from 'react';
+// import PhoneInput from 'react-phone-input-2';
 import * as Yup from 'yup';
 
 interface UserData {
@@ -25,11 +26,19 @@ const LoginSchema = Yup.object().shape({
 });
 
 function RegistrationForm() {
+  const [phone, setPhone] = useState('');
+
   const submitSignUp = async (
     values: UserData,
     { resetForm }: { resetForm: () => void },
   ) => {
-    await axios.post('https://formspree.io/f/mdovqnpy', values);
+    const userData = {
+      name: values.name,
+      phone: phone,
+      email: values.email,
+    };
+    await axios.post('https://formspree.io/f/mdovqnpy', userData);
+    setPhone('');
     resetForm();
   };
 
@@ -56,29 +65,32 @@ function RegistrationForm() {
             <ErrorMessage name="name" component="div" className="form__input__error" />
           </div>
           <div className="form__input">
-            <Field type="text" id="phone" name="phone">
-              {({ field, form }: FieldProps<string, FormikValues>) => (
-                <PhoneInput
-                  containerClass="form__input__field"
-                  inputStyle={{
-                    background: 'transparent',
-                    height: '100%',
-                    color: '#FFFFFF',
-                    border: 'none',
-                  }}
-                  buttonClass="test"
-                  buttonStyle={{
-                    background: 'transparent',
-                    border: 'none',
-                    color: 'black',
-                  }}
-                  placeholder="Ваш номер телефона"
-                  country="ua"
-                  value={field.value}
-                  onChange={(value) => form.setFieldValue('phone', value)}
-                />
-              )}
-            </Field>
+            {/* <PhoneInput
+              containerClass="form__input__field"
+              inputStyle={{
+                background: 'transparent',
+                height: '100%',
+                color: '#FFFFFF',
+                border: 'none',
+              }}
+              buttonClass="test"
+              buttonStyle={{
+                background: 'transparent',
+                border: 'none',
+                color: 'black',
+              }}
+              placeholder="Ваш номер телефона"
+              country="ua"
+              value={phone}
+              onChange={(e) => setPhone(e)}
+            /> */}
+            <Field
+              className="form__input__field"
+              placeholder="Ваш номер телефона"
+              type="text"
+              id="phone"
+              name="phone"
+            />
             <ErrorMessage name="phone" component="div" className="form__input__error" />
           </div>
           <div className="form__input">
